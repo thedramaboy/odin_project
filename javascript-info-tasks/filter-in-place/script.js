034b99c8a647e923f00600846c97d85626a0b85b
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2025-12-04 15:26:52 -0700

    Implemented filter in place

---
+++ javascript-info-tasks/filter-in-place/script.js
@@ -0,0 +1,11 @@
+function filterRangeInPlace(arr, a, b) {
+    for (let i = 0; i < arr.length; i++) {
+        if ( arr[i] < a || arr[i] > b) {
+            arr.splice(i, 1);
+        }
+    }
+}
+
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
