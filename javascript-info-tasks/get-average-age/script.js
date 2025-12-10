b4ea77555aecd18d47351eeeb786dd05dd5ae019
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2025-12-09 17:10:46 -0700

    Get average age

---
+++ javascript-info-tasks/get-average-age/script.js
@@ -0,0 +1,15 @@
+function getAverageAge(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].age;
+    }
+    return sum/ arr.length;
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+console.log(getAverageAge(arr));
