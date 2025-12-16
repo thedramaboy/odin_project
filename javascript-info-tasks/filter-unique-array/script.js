950ca55718e517df25fce06b39099f79833ae55c
thedramaboy <200464650+thedramaboy@users.noreply.github.com>
2025-12-15 19:12:08 -0700

    Filter unique array

---
+++ javascript-info-tasks/filter-unique-array/script.js
@@ -0,0 +1,15 @@
+function unique(arr) {
+    let uniqueArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueArr.includes(arr[i])) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+    return uniqueArr;
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log(unique(strings));
